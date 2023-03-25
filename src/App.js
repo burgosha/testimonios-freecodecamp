d6f733507cd21b5555cc780c427baa0e57424dc1
Hernán Burgos <122620259+burgosha@users.noreply.github.com>
2023-03-25 11:23:32 -0300

    Update App.js
    Utilización de fragmentos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Testimonio from './componentes/Testimonio';
+import { Fragment } from 'react';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
           imagen="shawn"
           cargo="Ingeniero de Software"
           empresa="Ingeniero de Software"
-          testimonio="Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida."
+          testimonio={<Fragment>Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. <strong> freeCodeCamp cambió mi vida</strong>.</Fragment>}
         />
         <Testimonio
           nombre="Sarah Chima"
@@ -21,7 +22,7 @@ function App() {
           imagen="sarah"
           cargo="Ingeniera de Software"
           empresa="ChatDesk"
-          testimonio="freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble."
+          testimonio={<Fragment><strong>freeCodeCamp fue la puerta de entrada a mi carrera</strong> como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.</Fragment>}
         />
         <Testimonio 
           nombre="Emma Bostian"
@@ -29,7 +30,7 @@ function App() {
           imagen="emma"
           cargo="Ingeniera de Software"
           empresa="Spotify"
-          testimonio="Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."
+          testimonio={<Fragment>Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en <strong>freeCodeCamp me dio las habilidades</strong> y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.</Fragment>}
         />
       </div>
     </div>
